5e5a42596ca3a095fea42700731f06c9f3b4f58a
Aakash <113512885+tcgconsolee@users.noreply.github.com>
2025-08-18 16:28:07 +0530

    backend 2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, redirect, url_for, request
 from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.mutable import MutableList
 
 app = Flask(__name__)
 
@@ -21,8 +22,10 @@ class Users(UserMixin, db.Model):
                          nullable=False)
     password = db.Column(db.String(250),
                          nullable=False)
-    queries = db.Column(db.String(250),
-                         nullable=False)
+    queries = db.Column(db.Text,
+                         nullable=False, default="")
+    bills = db.Column(MutableList.as_mutable(db.JSON),
+              default=[False, False, False, False])
  
  
 db.init_app(app)
@@ -79,11 +82,11 @@ def login():
 def aboutprez():
     return render_template("aboutprez.html")
 
-@app.route("/profile")
+@app.route("/profile", methods=["GET", "POST"])
 def profile():
     if not current_user.is_authenticated:
         return redirect(url_for("login"))
-    return render_template("profile.html", name = current_user.username)
+    return render_template("profile.html", name = current_user.username, queries = current_user.queries)
 
 @app.route("/logout")
 @login_required
@@ -91,12 +94,10 @@ def logout():
     logout_user()
     return redirect(url_for("index"))
 
-@app.route("/query")
+@app.route("/query", methods=["GET", "POST"])
 def query():
     if not current_user.is_authenticated:
         return redirect(url_for("login"))
-    if not current_user.queries:
-        current_user.queries = ""
     if request.method == "POST":
         current_user.queries = current_user.queries + f'''
             <div class="billgridrow">
@@ -117,8 +118,20 @@ def query():
                 </div>
             </div>
         '''
-        return render_template("query.html", queries="")
-    return render_template("query.html", queries="")
+        db.session.commit()
+        return redirect(url_for("profile"))
+    return render_template("query.html")
+
+@app.route("/bill", methods=["GET", "POST"])
+def bill():
+    n = request.args.get("n")
+    if not current_user.is_authenticated:
+        return redirect(url_for("login"))
+    if request.method == "POST":
+        current_user.bills[int(n)] = True
+        db.session.commit()
+        return redirect(url_for("profile"))
+    return render_template("bill.html")
 
 if __name__ == "__main__":
-    app.run(host ="0.0.0.0", port = 5500, debug=True   )
+    app.run(host ="0.0.0.0", port = 10000, debug=True   )
